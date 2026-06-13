3a2329d3fd40a960767d81eafcc9599b3b59fb51
Fhelo <fhelo3030@gmail.com>
2026-06-13 18:21:24 +0100

    Added sidebar component

---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,17 @@
+function Sidebar() {
+  return (
+    <div>
+      <h2>Menu</h2>
+
+      <ul>
+        <li>Dashboard</li>
+        <li>Employees</li>
+        <li>Departments</li>
+        <li>Reports</li>
+        <li>Settings</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Sidebar;
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,9 +1,17 @@
+import Sidebar from "../components/Sidebar";
+
 function Dashboard() {
   return (
     <div>
-      <h1>Employee Management Portal</h1>
-      <h2>Dashboard</h2>
-      <p>This is my CodeAlpha DevOps Task 1 Project.</p>
+      <Sidebar />
+
+      <div>
+        <h1>Employee Management Portal</h1>
+
+        <h2>Dashboard</h2>
+
+        <p>Welcome to My CodeAlpha DevOps Task 1 Project.</p>
+      </div>
     </div>
   );
 }
