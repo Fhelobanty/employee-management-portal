9106213b68984118a5b061c01a4f2738b255d6c6
Fhelo <fhelo3030@gmail.com>
2026-06-13 17:56:10 +0100

    Initial React frontend structure

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+function Dashboard() {
+  return (
+    <div>
+      <h1>Employee Management Portal</h1>
+      <h2>Dashboard</h2>
+      <p>This is my CodeAlpha DevOps Task 1 Project.</p>
+    </div>
+  );
+}
+
+export default Dashboard;
